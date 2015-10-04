11e701393874f25e08fd7a3e2e0816030bf60e9f
Adam Washington <rprospero@gmail.com>
2015-10-04 23:30:28 +0100

    Plot Data File

--- src/Main.hs
+++ src/Main.hs
@@ -3,23 +3,82 @@
 module Main where
 
 import Haste
-import Haste.DOM
+import Haste.DOM hiding (attr)
 import Haste.Events
 import Haste.Foreign
 import Data.IORef
 
 import Corfunc
+import Haste.D2.Scale
+import Haste.D2.Select
+import Haste.D2.SVG
+import Haste.D2.SVG.Axis as Ax
+import Prelude hiding (log)
+
+width = 400 :: Double
+height = 400 :: Double
+margin = 50 :: Double
+
 
 readAsText :: JSString -> ElemID -> IO ()
 readAsText = ffi "function(name,x){var r = new FileReader;r.onload=function(q){Haste[name](q.target.result)};r.readAsText(document.getElementById(x).files[0]);}"
 
-updatePage :: IORef [(Double,Double)] -> IO ()
-updatePage pref = do
+updatePage pref svgref = do
   ps <- readIORef pref
-  let xs = map fst ps
-      ys = map snd ps
+  (svg,l) <- readIORef svgref
+  let xs = map head ps
+      ys = map (!! 1) ps
+
+  _ <- append "g" svg
+      >>= attr "class" ("lines" :: String)
+      >>= attr "clip-path" ("url(#clip)" :: String)
+      >>= selectAll "path"
+      >>= d3data [ps]
+      >>= enter
+      >>= append "path"
+      >>= attr "d" l
+      >>= attr "style" ("stroke: black; fill: none" :: String)
+
   print xs
   print ys
+
+
+makeGraph = do
+  x <- log >>= domain [1e-3, 1]
+      >>= range [0,width]
+  y <- log >>= domain [1e-2, 1e5]
+      >>= range [height,0]
+
+  xAxis <- Ax.axis >>= Ax.scale x
+          >>= Ax.orient Ax.Bottom
+          >>= Ax.ticks 5
+
+  yAxis <- Ax.axis >>= Ax.scale y
+          >>= Ax.orient Ax.Left
+          >>= Ax.ticks 5
+
+  l <- line x y
+
+  svg <- select "body"
+        >>= append "svg"
+        >>= attr "width" (width + margin)
+        >>= attr "height" (height + margin)
+        >>= append "g"
+        >>= attr "transform" (translate (margin/2,margin/2))
+
+
+  _ <- append "g" svg
+      >>= attr "class" ("x axis" :: String)
+      >>= attr "transform" (translate (0,height))
+      >>= call xAxis
+
+
+  _ <- append "g" svg
+      >>= attr "class" ("y axis" :: String)
+      >>= call yAxis
+
+  return (svg,l)
+
 
 main :: IO ()
 main = do
@@ -27,16 +86,17 @@ main = do
 
   ps <- newIORef []
 
-  let action = updatePage ps
+  (svg,l) <- makeGraph
+  svgref <- newIORef (svg,l)
+
+  let action = updatePage ps svgref
 
   export "processDump" (processDump action ps)
   _ <- onEvent loadPath Change $ const $ readAsText "processDump" "loadPath"
   return ()
 
-lToTup [x,y] = (x,y)
-
-processDump :: IO () -> IORef [(Double,Double)] -> JSString -> IO ()
+processDump :: IO () -> IORef [[Double]] -> JSString -> IO ()
 processDump action ds result = do
-  let ps = map (lToTup . map read . words) . tail . lines . toString$result
+  let ps = map (map read . words) . tail . lines . toString$result
   writeIORef ds ps
   action
